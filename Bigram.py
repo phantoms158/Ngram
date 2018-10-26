f65f76cf0f860b0e6ca59d0e9231c3e16cdaa90b
trongpq <trongpq@runsystem.net>
2018-10-27 01:36:36 +0700

    Done Bigram

--- Bigram.py
+++ Bigram.py
@@ -17,10 +17,11 @@ class BigramLanguageModel:
                 previous_word = None
                 for word in words:
                     if previous_word != None:
-                        if previous_word != SENTENCE_START and word != SENTENCE_END:
+                        if word != SENTENCE_END:
                             self.counts[(previous_word +" "+ word)] = self.counts.get((previous_word +" " +word), 0) + 1
                             self.context_counts[previous_word] = self.context_counts.get(previous_word, 0) + 1
                             self.counts[word] = self.counts.get(word,0) + 1
+                            self.context_counts[""] = self.context_counts.get("",0) + 1
                     previous_word = word
             # print(self.counts)
             # print(self.context_counts)
@@ -29,19 +30,22 @@ class BigramLanguageModel:
                 # print(ngram, count)
                 tmp = ngram.split(" ")
                 tmp.pop()
-                context = " ".join(tmp)
-                print(context)
+                # print("".join(tmp))
+                context = "".join(tmp)
+                # print(context)
                 # print(self.context_counts)
                 if context in self.context_counts:
-                        # print(context)
-                    probability = float(self.counts[ngram]/self.context_counts[context])
-                    f.write(str(ngram) + "  " + str(probability)+"\n")
+                        print(context)
+                        probability = float(self.counts[ngram]/self.context_counts[context])
+                        f.write(str(ngram) + "  " + str(probability)+"\n")
 def Load_Model(file_path):
     probabilities  = dict()
     with open(file_path, "r") as f:
         for line in f:
             everyline = line.rstrip("\n")
+            # print(everyline)
             xs = everyline.split("  ")
+            # print(xs)
             probabilities[xs[0]] = xs[1]
         # print(probabilities)
     return probabilities
@@ -64,29 +68,24 @@ def Test_Bigram(file_test_path):
             # print(words)
             previous_word = None
             for word in words:
+                
                 if previous_word != None:
-                    if previous_word != SENTENCE_START and word != SENTENCE_END:
+                    if  word != SENTENCE_END:
+                        # print(word)
                         if word in probabilities:
+                            # print(word)
                             P1 = lamda1*float(probabilities[word]) + (1-lamda1) / V
-                            # print(probabilities[word])
-                            print(P1)
-                        # print(type(tamp))
-                        tamp = "".join(previous_word + " "  + word)
-                        if tamp in probabilities[previous_word + " "  + word]:
-                            # print("ok")
+                            # print(P1)
+                        tamp = [previous_word,word]
+                        # print(tamp)
+                        my_tamp = " ".join(tamp)
+                        if my_tamp in probabilities:
                             P2 = lamda2*float(probabilities[previous_word + " "  + word]) + (1-lamda2) * P1
                             # print(P2)
-                        # H += math.log(1 / P2, 2)
-                        # W += 1
+                        H += math.log(1 / P2, 2)
+                        W += 1
                 previous_word = word
-            print(H)
-            print(W)
-
-    # print(P1)
-    # print(P2)
-    # print(H)
-    # print(W)
-    # print("entropy = " + str(H / W))
+    print("entropy = " + str(H / W))
 
 if __name__ == '__main__':
     #
@@ -99,8 +98,9 @@ if __name__ == '__main__':
     # data_test_path = "./test/02-test-input.txt"
     data_train_wiki_path = "./data/wiki-en-train.word"
     data_test_wiki_path = "./data/wiki-en-test.word"
+    file_test_bigram = "./bigram_file_test.txt"
     data_test_bigram = "./bigram_data_test.txt"
-    dataset_model = BigramLanguageModel(data_train_path)
+    # dataset_model = BigramLanguageModel(data_train_path)
     # print(dataset_model)
-    # Test_Bigram(data_test_bigram)
-
+    Test_Bigram(data_test_bigram)
+    
